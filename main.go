150ce87a522f3ae673fc47ea2f77805ee38490ec
Ferhof <alexander.kiriltsev@gmail.com>
2026-02-19 22:28:26 +0300

    Add weather fetching and geolocation functionality

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"demo/weather/geo"
+	"demo/weather/weather"
+	"flag"
+	"fmt"
+)
 
+func main() {
+	fmt.Println("Новый проект")
+	city := flag.String("city", "", "Город пользователя")
+	format := flag.Int("format", 1, "Формат вывода погоды")
+
+	flag.Parse()
+
+	fmt.Println(*city)
+	geoData, err := geo.GetMyLocation(*city)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	weatherData, _ := weather.GetWeather(*geoData, *format)
+	fmt.Println(weatherData)
 }
